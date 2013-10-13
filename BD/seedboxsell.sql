72d62d04785bc1982e367e39352294c5162424ba
NGUYEN <huy.bao_nguyen@yahoo.fr>
2013-10-13 23:59:15 +0200

    Phase 3

---
+++ BD/seedboxsell.sql
@@ -0,0 +1,80 @@
+/*==============================================================*/
+/* Nom de SGBD :  MySQL 5.0                                     */
+/* Date de cr�ation :  11/09/2013 13:16:52                      */
+/*==============================================================*/
+
+/*==============================================================*/
+/* création de la base de données et de l'utilisateur */
+/*==============================================================*/
+
+create database if not exists seedbox;
+
+create user 'seedboxuser'@'localhost' identified by 'seedboxpass';
+
+GRANT ALL PRIVILEGES ON `seedbox` . * TO 'seedboxuser'@'localhost' WITH GRANT OPTION ;
+
+USE seedbox;
+
+drop table if exists T_FOURNISSEUR;
+
+drop table if exists T_SEEDBOX;
+
+drop table if exists T_TYPEABONN;
+
+/*==============================================================*/
+/* Table : T_FOURNISSEUR                                        */
+/*==============================================================*/
+create table T_FOURNISSEUR
+(
+   F_NUMFRS             int not null,
+   F_NOMFRS             varchar(20),
+   primary key (F_NUMFRS)
+);
+
+/*==============================================================*/
+/* Table : T_SEEDBOX                                            */
+/*==============================================================*/
+create table T_SEEDBOX
+(
+   SB_ID                int not null,
+   F_NUMFRS             int not null,
+   TA_CODEABONN         varchar(3) not null,
+   SB_NOM               varchar(20),
+   SB_DATEAJOUT         date,
+   SB_PRIX              decimal(5,2),
+   SB_IMAGE             varchar(40),
+   primary key (SB_ID)
+);
+
+/*==============================================================*/
+/* Table : T_TYPEABONN                                          */
+/*==============================================================*/
+create table T_TYPEABONN
+(
+   TA_CODEABONN         varchar(3) not null,
+   TA_LIBELLEABONN      varchar(30),
+   primary key (TA_CODEABONN)
+);
+
+alter table T_SEEDBOX add constraint FK_APPARTENIR foreign key (TA_CODEABONN)
+      references T_TYPEABONN (TA_CODEABONN) on delete restrict on update restrict;
+
+alter table T_SEEDBOX add constraint FK_FOURNIR foreign key (F_NUMFRS)
+      references T_FOURNISSEUR (F_NUMFRS) on delete restrict on update restrict;
+
+
+INSERT INTO T_TYPEABONN VALUES('A01', 'Abonnement sur 1 mois');
+INSERT INTO T_TYPEABONN VALUES('A06', 'Abonnement sur 6 mois');
+INSERT INTO T_TYPEABONN VALUES('A12', 'Abonnement sur 12 mois');
+INSERT INTO T_TYPEABONN VALUES('A27', 'Abonnement sur 27 mois');
+
+INSERT INTO T_FOURNISSEUR VALUES('01', 'OVH');
+INSERT INTO T_FOURNISSEUR VALUES('02', '1&1');
+INSERT INTO T_FOURNISSEUR VALUES('03', 'seedbox.fr');
+
+INSERT INTO T_SEEDBOX VALUES(001, '01', 'A01', 'H�bergement 1 mois', '10/09/2013', '6', ' ');
+INSERT INTO T_SEEDBOX VALUES(002, '01', 'A06', 'H�bergement 6 mois', '10/09/2013', '30', ' ');
+INSERT INTO T_SEEDBOX VALUES(003, '01', 'A12', 'H�bergement Gold', '10/09/2013', '50', ' ');
+INSERT INTO T_SEEDBOX VALUES(004, '02', 'A06', 'H�bergement 6 mois', '10/09/2013', '30', ' ');
+INSERT INTO T_SEEDBOX VALUES(005, '03', 'A12', 'H�bergement annuel', '10/09/2013', '60', ' ');
+INSERT INTO T_SEEDBOX VALUES(006, '03', 'A27', 'H�bergement 2 ans avec 3 mois offerts', '10/09/2013', '110', ' ');
--- accueil.php
+++ accueil.php
@@ -1,7 +1,74 @@
 <?php
+try {
+    require 'connexionbdd.inc';
+    $bdd = getBdd();
+    
+    //Affichage des catégorie
+    $requete = "SELECT FRS.F_NUMFRS, FRS.F_NOMFRS , COUNT(*) as 'nbpdt' FROM T_SEEDBOX SB JOIN T_FOURNISSEUR FRS ON SB.F_NUMFRS = FRS.F_NUMFRS GROUP BY SB_ID;";
+    $stmtCategories = $bdd->query($requete);
+    
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+} catch (Exception $e) {
+    $msgErreur = $e->getMessage() . '(' . $e->getFile() . ', ligne ' . $e->getLine() . ')';
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width,intial-scale=1.0">
+        <link rel="stylesheet" href="librairies/bootstrap/css/bootstrap.min.css" type="text/css"/>
+        <link rel="stylesheet" href="contenu/style.css" type="text/css"/>
+        <link rel="icon" type="text/css" href="contenu/Images/favicon.ico"/>
+    </head>
+
+
+    <body>
+
+        <nav class="navbar navbar-default" role="navigation">
+            <a class="navbar-brand" href="accueil.html"><span class="nom_site">SeedBox Sell</span></a>
+        </nav>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="msg_bienvenue">
+                    <strong class="msg">Bienvenue sur le site SeedBox Sell, la référence en matière de location de Seed Box.</strong> 
+                </p>
+            </div>
+        </div>
+        <div class="breadcrumb"><a href="accueil.html" class="first">Accueil</a></div>
+        <div class="container breadcrumb">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="list-group">
+                        <a class="list-group-item" href="accueil.html" title="Accueil">Accueil</a>
+                        <?php
+                        
+                        foreach ($stmtCategories as $ligne)
+                        {
+                            $libcat = $ligne['F_NOMFRS'];
+                            $nombre = $ligne['nbpdt'];
+                            
+                            echo "<a class='list-group-item' href='abonnement.html'>$libcat<span class='badge'>$nombre</span> </a>";
+                        }
+                        ?>
+                       
+                        <!--<a class="list-group-item" href="fournisseur.html" title="Fournisseurs">Fournisseurs<span class="badge">3</span></a>-->
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <img class="img-responsive img_accueil" src="contenu/Images/smtp-server.jpg"/>
+                </div>
+            </div>
+            <footer class="breadcrumb">
+                <div class="col-md-12">
+                    <span>COMLAR Rémi - NGUYEN Huy Bao</span> 
+                </div>
+            </footer>
+        </div>
+
+
+    </body>
+</html>
